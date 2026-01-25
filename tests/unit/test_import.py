8176bd8cd4b0f38c1dc5705ee40e615643b154dd
KUBOTA-KOKI <kubota.k.af@gmail.com>
2026-01-25 23:37:01 +0900

    冗長なtestを削除

--- tests/unit/test_import.py
+++ tests/unit/test_import.py
@@ -1,13 +1,6 @@
 """mykeibadb-python パッケージ初期化テスト."""
 
 
-def test_package_import() -> None:
-    """パッケージのインポートが正常に動作することを確認."""
-    import mykeibadb
-
-    assert mykeibadb is not None
-
-
 def test_package_has_version() -> None:
     """パッケージにバージョン情報が含まれることを確認."""
     import mykeibadb
