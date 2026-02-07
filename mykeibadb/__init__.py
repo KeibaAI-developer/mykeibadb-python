a836d969959bdb416fe08a1e9865ba77b7a2062a
KUBOTA-KOKI <kubota.k.af@gmail.com>
2026-02-08 00:17:23 +0900

    バージョンの記載を更新

--- mykeibadb/__init__.py
+++ mykeibadb/__init__.py
@@ -4,7 +4,12 @@
 競馬データを取得するための統一的なインターフェースを提供します。
 """
 
-__version__ = "0.1.0"
+try:
+    from importlib.metadata import version
+
+    __version__ = version("mykeibadb-python")
+except Exception:
+    __version__ = "unknown"
 
 from mykeibadb.config import ConfigManager
 from mykeibadb.connection import ConnectionManager
